033eff20d2b65c87fbf6ab1f8db7b322e37c794c
William Rafael Raymundo <william.raymundo.contato@gmail.com>
2021-09-26 20:14:09 -0300

    Read DetalhesConsulta

--- index/read/detalhesHistorico.php
+++ index/read/detalhesHistorico.php
@@ -1,7 +1,30 @@
 <?php
+$user = "root"; 
+$password = ""; 
+$database = "licence"; 
+
+# O hostname deve ser sempre localhost 
+$hostname = "localhost"; 
+
+# Conecta com o servidor de banco de dados 
+$conexao = mysqli_connect( $hostname, $user, $password ) or die( ' Erro na conexão ' ); 
+mysqli_select_db($conexao, $database);
+$query = "SELECT * FROM tipousuario"; 
+$result_query = mysqli_query($conexao, $query ) or die(' Erro na query:' );
 ?>
 <html>
     <body>
-        <h1>Olá</h1>
+    <label>Tipo Usuário</label>
+  <select class="browser-default">
+  <?php
+
+# Exibe os registros na tela 
+while ($row = mysqli_fetch_array( $result_query )) 
+{ 
+  ?><option value="<?php print $row['nivelPermi']; ?>">  <?php print $row['nomeCat']; ?> </option> <?php
+}
+
+?>
+ </select>
     </body>
 </html>
