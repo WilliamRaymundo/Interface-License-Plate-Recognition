261e483e54a798196ac863f6689e77affaa4c55a
William Rafael Raymundo <william.raymundo.contato@gmail.com>
2021-09-27 22:19:59 -0300

    Read detalhes historico

--- index/read/detalhesHistorico.php
+++ index/read/detalhesHistorico.php
@@ -10,27 +10,24 @@ $hostname = "localhost";
 # Conecta com o servidor de banco de dados 
 $conexao = mysqli_connect( $hostname, $user, $password ) or die( ' Erro na conexão ' ); 
 mysqli_select_db($conexao, $database);
-$query = "SELECT * FROM tipousuario"; 
+$query = "SELECT * FROM historico WHERE id = $id"; 
 $result_query = mysqli_query($conexao, $query ) or die(' Erro na query:' );
 
 ?>
 <html>
     <body>
-        <?php
-        echo $id; 
-        ?>
+    <button class="waves-effect waves-teal btn-flat" ><i class="material-icons" onclick="fecharPagUsu()">close</i></button>
     <h3>Detalhes Histórico</h3>
-    <label>Tipo Usuário</label>
-  <select class="browser-default">
-  <?php
-
-# Exibe os registros na tela 
-while ($row = mysqli_fetch_array( $result_query )) 
-{ 
-  ?><option value="<?php print $row['nivelPermi']; ?>">  <?php print $row['nomeCat']; ?> </option> <?php
-}
-
-?>
- </select>
+    <?php $row = mysqli_fetch_array( $result_query ) ?>
+    <div class="con-nit">
+      <table><tr>
+      <tr> <td>Entrada</td> <td><?php print $row['Entrada']; ?></td></tr>
+      <tr> <td> Saída</td> <td> <?php print $row['Saida']; ?></td></tr>
+      <tr> <td> Placa </td> <td><?php print $row['capPlaca']; ?></td></tr>
+      <tr> <td> Permissão </td> <td><?php print $row['Permi']; ?></td></tr>
+      <tr> <td> Captura </td> <td><img src="<?php print $row['captura']; ?>"></td></tr>
+      
+      </table>
+    </div>
     </body>
 </html>
