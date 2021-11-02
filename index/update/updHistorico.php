012b7c62e3c1a804e9ddd5d1c0cce541b5b5c69e
WilliamRaymundo <william.raymundo.contato@gmail.com>
2021-11-01 23:00:38 -0300

    upd Historico

--- index/update/updHistorico.php
+++ index/update/updHistorico.php
@@ -6,4 +6,7 @@ $database = "licence";
 
 # O hostname deve ser sempre localhost 
 $hostname = "localhost"; 
+$conexao = mysqli_connect( $hostname, $user, $password ) or die( ' Erro na conexão ' ); 
+mysqli_select_db($conexao, $database);
+$query = "SELECT * FROM historico WHERE id = $id"; 
 ?>
